0e33bc21959a60852e7dd2f1752011a0534192f4
abdullateef97 <adeniranyusufabdullateef@gmail.com>
2018-01-15 12:11:52 +0100

    a

---
+++ crawler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            startCrawler();
+        }
+
+        private static async Task startCrawler()
+        {
+            var url = "https://www.xpau.se";
+
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var contentDiv = htmlDoc.DocumentNode
+                .Descendants("div").SingleOrDefault(node => node.GetAttributeValue("class", "").Equals("content"));
+
+            Console.WriteLine(contentDiv);
+
+        }
+    }
+}
